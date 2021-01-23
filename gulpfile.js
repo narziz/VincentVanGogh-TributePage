766018deee4190343cd069704f4bd433c8efeb17
Narmin Aziz <narmin.aziz23@gmail.com>
2021-01-23 18:22:30 +0400

    adding project to github

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const { watch, series, src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+
+function sassToCss(done){
+  return src('src/scss/style.scss')
+        .pipe(sass())
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
+};
+
+function imageMinify(){
+  return src('src/img/*.+(png|jpg|jpeg|svg|gif)')
+        .pipe(imagemin())
+        .pipe(dest('dist/img'))
+};
+
+function copyFont(){
+  return src('src/font/*')
+        .pipe(dest('dist/font'))
+};
+
+function watchTasks(){
+
+  browserSync.init({
+        server: "./",
+        notify: false
+    });
+
+  watch('src/scss/*.scss', series('sass'));
+  watch(['./*.html', 'dist/*.css']).on('change', browserSync.reload);
+};
+
+exports.sass = sassToCss;
+exports.watch = series(sassToCss, imageMinify, copyFont, watchTasks);
